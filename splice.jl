b4a145a6b0db40c49de07b3fc2e7ca794965f1c0
Espinosa Mireles de Villafranca Alonso <espinoa5@t21303-la020.org.aalto.fi>
2021-02-19 16:20:59 +0200

    splice function has now been replaced completely. It now uses sparse arrays (accodrding to LightGraphs documentation though, performance improvements might be possible by using transpose of sparse transpose...). Also, the function has been split into a function that draws a sample and then splices on the actual vertex. It is easier to play aroung with specific MicroStates and easier to debu this way. Must check that mh_evolve in other files still playes nice with splice thought!

--- splice.jl
+++ splice.jl
@@ -1,73 +1,10 @@
-# TODO: Splicing seems to be returning paths with repeated edges. This must be urgently fixed. At the moment it seems to only happen when the protected  path segments disconnect the spliced vertex from the anchors. Also, for debugging purposes it seems smarter to have a splicing routine separate from the drawing routine.
+# TODO: Test more extensively to check that no cycles are introduced (I am pretty sure this is no longer happening)
 
 """
-Generates new proposed state from a given microstate by doing the splicing
-procedure in Flötteröd & Bierlaire 2013 (TR: Part B).
+Draw a vertex for splicing, given a probability distribution as a vector over the vertices of a graph.
 
-    splice(state, g, p_insert)
-    
-    state:      Micro state (made up of Γ, a, b, c)
-    g:          Graph
-    p_insert:   Probability distribution over nodes in g
-
-Given a micro state (i.e. a Path and anchor points a, c and pivot b) generates
-a proposed state (potentially the same) for a transition in the MH algorithm.
+    draw_insertion_vertex(state::MicroState, g, p_insert)
 """
-function splice(state::MicroState, g, p_insert)
-
-    insertion_set = setdiff(vertices(g),
-                            state.Γ[1:state.a],
-                            state.Γ[state.c:end])
-    
-    p_ν = p_insert[insertion_set]
-    ν = sample(insertion_set, p_ν) # insertion node
-    
-   
-    #TODO this should be checked very well...
-     # Distance matrix with reduced node set (set Inf distance)
-    dist_mat = copy(LightGraphs.weights(g))
-    excluded_nodes = cat(state.Γ[1:state.a-1], state.Γ[state.c:end], dims=1)
-    
-    for i in excluded_nodes
-        for j in outneighbors(g, i)
-            dist_mat[i,j] = Inf
-        end
-        for j in inneighbors(g, i)
-            dist_mat[j,i] = Inf
-        end
-    end
-    
-    ds = dijkstra_shortest_paths(g, state.Γ[state.a], dist_mat)
-    paths1 = enumerate_paths(ds, ν)
-    if length(paths1) == 0
-        return state
-    end
-    Γ₁ = paths1
-    
-    ds = dijkstra_shortest_paths(g, ν, dist_mat)
-    paths2 = enumerate_paths(ds, state.Γ[state.c])
-    if length(paths2) == 0 
-        return state
-    end
-    Γ₂ = paths2
-    
-    if length(intersect(Γ₁, Γ₂)) > 1 # Cycle in new path
-        return state
-    end
-    
-    new_Γ = cat(state.Γ[1:state.a-1], Γ₁[1:end-1], Γ₂, state.Γ[state.c+1:end], dims=1)
-    new_a = state.a
-    new_b = findfirst(isequal(ν), new_Γ)
-    new_c = new_b + length(Γ₂) - 1
-    
-    MicroState(new_Γ, new_a, new_b, new_c)
-end
-
-splice(state::MicroState, mh::MHInstance) = splice(state, mh.g, mh.p_insert)
-
-
-######################################################################
-
 function draw_insertion_vertex(state::MicroState, g, p_insert)
     insertion_set = setdiff(vertices(g),
                             state.Γ[1:state.a],
@@ -78,7 +15,20 @@ function draw_insertion_vertex(state::MicroState, g, p_insert)
 end
 
 
-function splice_sparse(state::MicroState, g::AbstractGraph, ν::Int)
+"""
+Generates new proposed state from a given microstate by doing the splicing
+procedure in Flötteröd & Bierlaire 2013 (TR: Part B).
+
+    splice(state::MicroState, g::AbstractGraph, ν::Int)
+    
+    state:      Micro state (made up of Γ, a, b, c)
+    g:          Graph
+    ν:          Vertex index of graph to splice to
+
+Given a micro state (i.e. a Path and anchor points a, c and pivot b) generates
+a proposed state (potentially the same) for a transition in the MH algorithm.
+"""
+function splice(state::MicroState, g::AbstractGraph, ν::Int)
 
     # 1 reduced node set for each splice segment
     dist_mat_1 = adjacency_matrix(g)
@@ -144,70 +94,27 @@ function splice_sparse(state::MicroState, g::AbstractGraph, ν::Int)
     MicroState(new_Γ, new_a, new_b, new_c)
 end
 
-
-
-
-
-
-
-
 """
-Same as splice but uses a sparse dist_mat for dijkstra shortest paths
-"""
-function splice_sp(state::MicroState, g, p_insert)
+Generates new proposed state from a given microstate by doing the splicing
+procedure in Flötteröd & Bierlaire 2013 (TR: Part B).
 
-    insertion_set = setdiff(vertices(g),
-                            state.Γ[1:state.a],
-                            state.Γ[state.c:end])
+    splice(state, g, p_insert)
     
-    p_ν = p_insert[insertion_set]
-    ν = sample(insertion_set, p_ν) # insertion node
-    
-   
-    #TODO this should be checked very well...
-     # Distance matrix with reduced node set (set Inf distance)
-    #dist_mat = copy(LightGraphs.weights(g))
-    dist_mat = adjacency_matrix(g)
-    excluded_nodes = cat(state.Γ[1:state.a-1], state.Γ[state.c:end], dims=1)
-    
-    for i in excluded_nodes
-        for j in outneighbors(g, i)
-            dist_mat[i,j] = Inf
-        end
-        for j in inneighbors(g, i)
-            dist_mat[j,i] = Inf
-        end
-    end
-    
-    ds = dijkstra_shortest_paths(g, state.Γ[state.a], dist_mat)
-    paths1 = enumerate_paths(ds, ν)
-    if length(paths1) == 0
-        # I guess that this should actually return an error?
-        return state
-    end
-    Γ₁ = paths1
-    
-    ds = dijkstra_shortest_paths(g, ν, dist_mat)
-    paths2 = enumerate_paths(ds, state.Γ[state.c])
-    if length(paths2) == 0 
-        return state
-    end
-    Γ₂ = paths2
-    
-    if length(intersect(Γ₁, Γ₂)) > 1 # Cycle in new path
-        return state
-    end
-    
-    new_Γ = cat(state.Γ[1:state.a-1], Γ₁[1:end-1], Γ₂, state.Γ[state.c+1:end], dims=1)
-    new_a = state.a
-    new_b = findfirst(isequal(ν), new_Γ)
-    new_c = new_b + length(Γ₂) - 1
-    
-    MicroState(new_Γ, new_a, new_b, new_c)
+    state:      Micro state (made up of Γ, a, b, c)
+    g:          Graph
+    p_insert:   Probability distribution over nodes in g
+
+Given a micro state (i.e. a Path and anchor points a, c and pivot b) generates
+a proposed state (potentially the same) for a transition in the MH algorithm.
+"""
+function splice(state::MicroState, g::AbstractGraph, p_insert::AbstractArray)
+    # Draw vertex
+    ν = draw_insertion_vertex(state, g, p_insert)
+    # Splice
+    splice(state, g, ν)
 end
 
-
-######################################################################
+splice(state::MicroState, mh::MHInstance) = splice(state, mh.g, mh.p_insert)
 
 
 """
